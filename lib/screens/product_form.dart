d3012c0dc71692f8cf06cffe3205c04e968b1062
andreast12 <andreast12.id@gmail.com>
2024-11-20 11:23:48 +0700

    menghapus comment todo

--- lib/screens/product_form.dart
+++ lib/screens/product_form.dart
@@ -126,14 +126,12 @@ class _ProductFormPageState extends State<ProductFormPage> {
                   onPressed: () async {
                     if (_formKey.currentState!.validate()) {
                       // Kirim ke Django dan tunggu respons
-                      // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
                       final response = await request.postJson(
                         "http://127.0.0.1:8000/create-flutter/",
                         jsonEncode(<String, String>{
                           'name': _name,
                           'price': _amount.toString(),
                           'description': _description,
-                          // TODO: Sesuaikan field data sesuai dengan aplikasimu
                         }),
                       );
                       if (context.mounted) {
--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -50,9 +50,8 @@ class ItemCard extends StatelessWidget {
               MaterialPageRoute(builder: (context) => const ProductPage()),
             );
           } else if (item.name == "Logout") {
-            final response = await request.logout(
-                // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
-                "http://127.0.0.1:8000/auth/logout/");
+            final response =
+                await request.logout("http://127.0.0.1:8000/auth/logout/");
             String message = response["message"];
             if (context.mounted) {
               if (response['status']) {
